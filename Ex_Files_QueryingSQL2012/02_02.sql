f44b55bec5d2da015c967861b9d762224a21d2d1
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-10-26 10:31:36 -0400

    used table and column aliases to reduce workload

--- Ex_Files_QueryingSQL2012/02_02.sql
+++ Ex_Files_QueryingSQL2012/02_02.sql
@@ -1,16 +1,21 @@
 USE AdventureWorks2012;
 GO
 
+-- Aliases (shorter representations of table names make it easier 
+-- to query tables) 
+
 -- Write a SELECT statement for the Employee table
 -- selecting specific attributes and using an alias for the table
-SELECT hre.LoginID, hre.JobTitle, hre.HireDate
-FROM HumanResources.Employee AS hre;
+SELECT e.LoginID, e.JobTitle, e.HireDate, e.Gender
+FROM HumanResources.Employee AS e;
 GO
 
 
 -- Write a SELECT statement for the Employee table
 -- selecting specific attributes and using an alias for the table
 -- but include aliases for the attribute column names as well
+-- use aliases to change column names in output to 
+-- make them a bit easier to read 
 SELECT hre.LoginID AS [login], hre.JobTitle AS [Title],hre.HireDate AS [Date Hired]
 FROM HumanResources.Employee AS hre;
 GO
