2c4ea76fef030011b2cb55cecf74f34e4e71b59c
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-10-27 15:16:28 -0400

    another example with distinct values

--- Ex_Files_QueryingSQL2012/03_01.sql
+++ Ex_Files_QueryingSQL2012/03_01.sql
@@ -1,6 +1,9 @@
 USE AdventureWorks2012;
 GO
 
+-- DISTINCT keyword reduces number of duplicates in 
+-- Query but doesn't eliminate them all together. 
+
 -- We want to see how many different types of people
 -- are represented in the Person table.
 -- Return all records and all attributes from the 
@@ -10,12 +13,16 @@ SELECT *
 FROM Person.Person;
 GO
 
+
 -- Ok, then let's only return the PersonType attribute
 -- Still too many records to search through
 SELECT PersonType
 FROM Person.Person;
 GO
 
+SELECT DISTINCT PersonType
+FROM Person.Person; 
+GO
 
 -- Use the DISTINCT keyword to return only distinct
 -- values from the PersonType field
@@ -29,3 +36,16 @@ GO
 SELECT DISTINCT PersonType, EmailPromotion
 FROM Person.Person;
 GO
+
+-- This is because there are different combinations of Email Promotion for Person Type. 
+-- Try alternative form.
+SELECT DISTINCT EmailPromotion AS [Email Promotion], PersonType
+FROM Person.Person;
+GO 
+
+-- Another example with DISTINCT
+-- Both examples tried don't result in reduced rows 
+-- Because these are already distinct. 
+SELECT DISTINCT Name, ModifiedDate AS [Date Modified] 
+FROM Person.CountryRegion;
+GO 
