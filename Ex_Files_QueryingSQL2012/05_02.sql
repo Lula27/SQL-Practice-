d34a39e32b44b13846226fcd4cf8badb9cc27c7f
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-11-06 16:02:48 -0500

    Change the order of the records by ordering -- the results on BusinessEntityID

--- Ex_Files_QueryingSQL2012/05_02.sql
+++ Ex_Files_QueryingSQL2012/05_02.sql
@@ -2,56 +2,49 @@
 -- 05_02_Sorting_with_ORDER_BY.sql
 --**********************************************
 
+
 USE AdventureWorks2012;
 GO
 
 -- Straightforward query with no order specified.
 -- Records are returned in the order the 
 -- database engine determines
-SELECT BusinessEntityID, FirstName, MiddleName, 
-		LastName
-FROM Person.Person;
-GO
+-- 1. Select BE_ID, FN, MN, LN from Person.Person tbl
+SELECT P.BusinessEntityID, P.FirstName,P.MiddleName, P.LastName 
+FROM Person.Person P; 
+GO 
 
 
 -- Change the order of the records by ordering 
 -- the results on BusinessEntityID
-SELECT BusinessEntityID, FirstName, MiddleName, 
-		LastName
-FROM Person.Person
-ORDER BY BusinessEntityID ASC;
-GO
+-- 2. Same as one but order using BE_ID from lowest to highest
+SELECT P.BusinessEntityID, P.FirstName,P.MiddleName, P.LastName 
+FROM Person.Person P
+ORDER BY P.BusinessEntityID; 
+GO 
 
--- Change the order of the records by ordering the 
+-- 3. Change the order of the records by ordering the 
 -- results in reverse order based on the last 
 -- name field.
-SELECT BusinessEntityID, FirstName, MiddleName, 
-		LastName
-FROM Person.Person
-ORDER BY LastName DESC;
-GO
+ 
 
--- Change the order of the records by ordering 
+
+-- 4. Change the order of the records by ordering 
 -- the results alphabetically based on the first 
 -- name and the last name with the first name 
 -- taking precedence.
-SELECT FirstName, MiddleName, LastName
-FROM Person.Person
-ORDER BY FirstName, LastName;
-GO
 
--- Change the order of the records by ordering the 
+
+-- 5. Change the order of the records by ordering the 
 -- results alphabetically based on the first name 
 -- to show the difference between an ORDER BY
 -- used on the first name only as opposed to first 
 -- then last.
 -- Note the order, or lack of order, for the 
 -- last names in this version
-SELECT FirstName, MiddleName, LastName
-FROM Person.Person
-ORDER BY FirstName;
-GO
 
+
+-- 6. Avoid using this method - gets messy! 
 -- Change the order of the records by ordering the 
 -- results alphabetically based on the first name 
 -- and then last name but in this instance, do not 
@@ -59,12 +52,10 @@ GO
 -- positions to show how the results sort.
 -- NOTE, this is not a good practice and is only 
 -- demonstrated for awareness
-SELECT FirstName, MiddleName, LastName
-FROM Person.Person
-ORDER BY 1, 3;
-GO
+-- FN(1), MN(2), LN(3)
 
--- Change the order of the records by ordering the 
+
+-- 7. Change the order of the records by ordering the 
 -- results based on the BusinessEntityID that 
 -- isn't included in the output to show that the 
 -- order is still applied even though the field
@@ -72,18 +63,11 @@ GO
 -- It's also hard to tell how the order was 
 -- factored so this could cause confusion in the 
 -- results.
-SELECT FirstName, MiddleName, LastName
-FROM Person.Person
-ORDER BY BusinessEntityID;
-GO
 
--- Change the order of the records by ordering the 
+
+-- 8. Change the order of the records by ordering the 
 -- results alphabetically based on the middle name 
 -- to show how the NULL impacts the results.
 -- NULL affects the results because it is unknown 
 -- and will show up in the results ahead of any 
 -- other value.
-SELECT FirstName, MiddleName, LastName
-FROM Person.Person
-ORDER BY MiddleName;
-GO
