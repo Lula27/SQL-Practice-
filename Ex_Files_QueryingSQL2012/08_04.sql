9d97afc221f54ff829e8c45bed11f4def4e1ad5c
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-01-20 21:52:31 -0500

    catch and throw error

--- Ex_Files_QueryingSQL2012/08_04.sql
+++ Ex_Files_QueryingSQL2012/08_04.sql
@@ -18,6 +18,23 @@ BEGIN CATCH
 END CATCH;
 GO
 
+-- Work with TRY/CATCH
+BEGIN TRY 
+-- Generate a divide-by-zero error
+	SELECT 1/0;
+END TRY 
+BEGIN CATCH 
+	SELECT 
+		ERROR_NUMBER() AS ErrorNumber
+		,ERROR_SEVERITY() AS ErrorSeverity
+		,ERROR_STATE() AS ErrorState
+		,ERROR_PROCEDURE() AS ErrorState
+		,ERROR_PROCEDURE() AS ErrorProcedure
+		,ERROR_LINE() AS ErrorLine
+		,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH;
+GO 
+
 
 
 -- Here we show a more concrete example of using TRY/CATCH
