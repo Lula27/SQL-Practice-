838460316d826025acb8404f4f977d92f6b26675
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-01-20 22:07:06 -0500

    catch and throw errors

--- Ex_Files_QueryingSQL2012/08_04.sql
+++ Ex_Files_QueryingSQL2012/08_04.sql
@@ -65,3 +65,23 @@ END CATCH;
 IF @@TRANCOUNT > 0
     COMMIT TRANSACTION;
 GO
+
+
+-- Transaction for try and catch error
+BEGIN TRY 
+-- Generate constraint violation error
+	DELETE FROM Production.Product
+	WHERE ProductID = 980;
+END TRY 
+BEGIN CATCH 
+	SELECT 
+		ERROR_NUMBER() AS ErrorNumber
+		,ERROR_SEVERITY() AS ErrorSeverity
+		,ERROR_STATE() AS ErrorState
+		,ERROR_PROCEDURE() AS ErrorProcedure
+		,ERROR_LINE() AS ErrorLine
+		,ERROR_MESSAGE() AS ErrorMessage; 
+
+	IF @@TRANCOUNT > 0 
+		ROLLBACK TRANSACTION;
+END CATCH; 
