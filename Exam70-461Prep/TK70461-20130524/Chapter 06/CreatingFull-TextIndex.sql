659ac7ba0d947ccc090fbf79f7b336627a0c6cce
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-06-11 17:50:03 -0400

    imported four documents

--- Exam70-461Prep/TK70461-20130524/Chapter 06/CreatingFull-TextIndex.sql
+++ Exam70-461Prep/TK70461-20130524/Chapter 06/CreatingFull-TextIndex.sql
@@ -1,10 +1,12 @@
 -- Exercise 1: Create a Table and Full-Text Components 
+USE TSQL2012; 
+GO 
 -- Check to see if Full-Text Search is installed 
 
-
+--1. Check to see if Full Text is installed 
 SELECT SERVERPROPERTY('IsFullTextInstalled'); -- assuming 1 means yes! 
-
--- Create table to use for full-text search: 
+GO 
+-- 2. Create table to use for full-text search: 
 CREATE TABLE dbo.Documents 
 ( 
 id INT IDENTITY(1,1) NOT NULL, 
@@ -15,5 +17,49 @@ doccontent VARBINARY(MAX) NOT NULL,
 CONSTRAINT PK_Documents 
 	PRIMARY KEY CLUSTERED(id) 
 ); 
+GO 
 
--- Import four documents included in the folder for this book 
+-- 3. Import four documents included in the folder for this book (Insert data) 
+INSERT INTO dbo.Documents
+(title, doctype, docexcerpt, doccontent)
+SELECT N'Columnstore Indices and Batch Processing', 
+ N'docx',
+ N'You should use a columnstore index on your fact tables,
+   putting all columns of a fact table in a columnstore index. 
+   In addition to fact tables, very large dimensions could benefit 
+   from columnstore indices as well. 
+   Do not use columnstore indices for small dimensions. ',
+ bulkcolumn
+FROM OPENROWSET(BULK 'C:\Users\loret\Desktop\DataSciencePrep\SQL\SQL-Practice-\Exam70-461Prep\TK70461-20130524\Chapter 06\ColumnstoreIndicesAndBatchProcessing.docx', 
+                SINGLE_BLOB) AS doc;
+INSERT INTO dbo.Documents
+(title, doctype, docexcerpt, doccontent)
+SELECT N'Introduction to Data Mining', 
+ N'docx',
+ N'Using Data Mining is becoming more a necessity for every company 
+   and not an advantage of some rare companies anymore. ',
+ bulkcolumn
+FROM OPENROWSET(BULK 'C:\Users\loret\Desktop\DataSciencePrep\SQL\SQL-Practice-\Exam70-461Prep\TK70461-20130524\Chapter 06\IntroductionToDataMining.docx', 
+                SINGLE_BLOB) AS doc;
+INSERT INTO dbo.Documents
+(title, doctype, docexcerpt, doccontent)
+SELECT N'Why Is Bleeding Edge a Different Conference', 
+ N'docx',
+ N'During high level presentations attendees encounter many questions. 
+   For the third year, we are continuing with the breakfast Q&A session. 
+   It is very popular, and for two years now, 
+   we could not accommodate enough time for all questions and discussions! ',
+ bulkcolumn
+FROM OPENROWSET(BULK 'C:\Users\loret\Desktop\DataSciencePrep\SQL\SQL-Practice-\Exam70-461Prep\TK70461-20130524\Chapter 06\WhyIsBleedingEdgeADifferentConference.docx', 
+                SINGLE_BLOB) AS doc;
+INSERT INTO dbo.Documents
+(title, doctype, docexcerpt, doccontent)
+SELECT N'Additivity of Measures', 
+ N'docx',
+ N'Additivity of measures is not exactly a data warehouse design problem. 
+   However, you have to realize which aggregate functions you will use 
+   in reports for which measure, and which aggregate functions 
+   you will use when aggregating over which dimension.',
+ bulkcolumn
+FROM OPENROWSET(BULK 'C:\Users\loret\Desktop\DataSciencePrep\SQL\SQL-Practice-\Exam70-461Prep\TK70461-20130524\Chapter 06\AdditivityOfMeasures.docx', 
+                SINGLE_BLOB) AS doc;
