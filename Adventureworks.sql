907fb511a835c74f7802cd76b0a88d6b748cf741
Loretta Agyemang <loretta.agyemang@gmail.com>
2020-03-04 13:09:19 -0500

    practice with adventureworks

---
+++ Adventureworks.sql
@@ -0,0 +1,13 @@
+USE AdventureWorks2012;
+
+-- Practice Resource: https://sqlzoo.net/wiki/AdventureWorks 
+-- https://docs.microsoft.com/en-us/previous-versions/tn-archive/cc546512%28v%3dtechnet.10%29
+-- Quora: https://www.quora.com/I-have-the-database-AdventureWorks-Is-there-a-website-full-of-advanced-SQL-query-practice-questions-which-I-can-use-to-query-against-the-AdventureWorks-database
+-- SQL skills: https://www.sqlskills.com/
+
+
+
+
+-- 1. Show Company Name for James D. Kramer
+-- Use Customer table 
+
